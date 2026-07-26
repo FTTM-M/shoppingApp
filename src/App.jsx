48fcdebaed6cb5dd23f09973eb913411b8c1ebd2
FTTM <fatemehalimirzaee14@gmail.com>
2026-07-26 11:40:07 +0330

    created the routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,22 @@
-function App() {
-  return(
-    <>
-    <h1>Shop</h1>
-    </>
-  )
- 
-  
+import { Navigate, Route, Routes } from "react-router-dom";
 
+import Products from "./pages/Products";
+import CheckOut from "./pages/CheckOut";
+import NotFound from "./pages/404";
+import Details from "./pages/Details";
+
+function App() {
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<Navigate to="/products" replace />} />
+        <Route path="/products" element={<Products />} />
+        <Route path="/chackOut" element={<CheckOut />} />
+        <Route path="/products/:id" element={<Details />} />
+        <Route path="/*" element={<NotFound />} />
+      </Routes>
+    </>
+  );
 }
 
-export default App
+export default App;
