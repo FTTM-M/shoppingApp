af46e9582810a4d2870f20075652b20e9f87e89b
FTTM <fatemehalimirzaee14@gmail.com>
2026-07-27 11:17:44 +0330

    rendered  the list of products

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -1,9 +1,21 @@
 import { useProducts } from "../context/ProductContext";
 
+
+import styles from "./Product.module.css"
+
 function Products() {
   const products = useProducts();
-  console.log(products);
-  return <div>Products</div>;
+  // console.log(products);
+  return (
+    <div className={styles.container}>
+      {!products.length && <p>Loading ...</p>}
+      <div className={styles.product}>
+        {products.map((product) => (
+          <p key={product.id}>{product.title}</p>
+        ))}
+      </div>
+    </div>
+  );
 }
 
 export default Products;
