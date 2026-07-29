89cdfb93c4c46a74231f36f855cdd622a5036fa4
FTTM <fatemehalimirzaee14@gmail.com>
2026-07-29 12:45:24 +0330

    set the categories box

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -5,6 +5,7 @@ import styles from "./Product.module.css";
 import Loading from "../components/Loading";
 import { ImSearch } from "react-icons/im";
 import { useState } from "react";
+import { FaListUl } from "react-icons/fa";
 
 function Products() {
   const [search, setSerach] = useState("");
@@ -13,6 +14,16 @@ function Products() {
 
   const ButtonHandler = () => {
     console.log(search);
+  };
+
+  const CategoriesHandler = (event) => {
+    const { tagName } = event.target;
+
+    const category = event.target.innerText.toLowerCase();
+
+    if (tagName !== "LI") return;
+
+    console.log(category);
   };
 
   return (
@@ -34,6 +45,19 @@ function Products() {
           {products.map((product) => (
             <ProductCards key={product.id} data={product} />
           ))}
+        </div>
+        <div>
+          <div>
+            <FaListUl />
+            <p>Categories</p>
+          </div>
+          <ul onClick={CategoriesHandler}>
+            <li>All</li>
+            <li>Electronics</li>
+            <li>jewelery</li>
+            <li>Man's Clothing</li>
+            <li>Woman's Clothing</li>
+          </ul>
         </div>
       </div>
     </>
