98b7f0a29b8d0515a437140f462a863df57f0b57
FTTM <fatemehalimirzaee14@gmail.com>
2026-07-29 11:13:08 +0330

    set the loading

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -1,17 +1,18 @@
 import { useProducts } from "../context/ProductContext";
 import ProductCards from "../components/ProductCards";
 
-import styles from "./Product.module.css"
+import styles from "./Product.module.css";
+import Loading from "../components/Loading";
 
 function Products() {
   const products = useProducts();
   // console.log(products);
   return (
-    <div className={styles.container}>
-      {!products.length && <p>Loading ...</p>}
+    <div>
+      {!products.length && <Loading />}
       <div className={styles.product}>
         {products.map((product) => (
-         <ProductCards  key={product.id} data={product}/>
+          <ProductCards key={product.id} data={product} />
         ))}
       </div>
     </div>
