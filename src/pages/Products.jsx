d64002ba3e99c7dce603aa38ce78077e13746b3d
FTTM <fatemehalimirzaee14@gmail.com>
2026-07-29 11:51:19 +0330

    set the search box

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -3,20 +3,40 @@ import ProductCards from "../components/ProductCards";
 
 import styles from "./Product.module.css";
 import Loading from "../components/Loading";
+import { ImSearch } from "react-icons/im";
+import { useState } from "react";
 
 function Products() {
+  const [search, setSerach] = useState("");
   const products = useProducts();
   // console.log(products);
+
+  const ButtonHandler = () => {
+    console.log(search);
+  };
+
   return (
-    <div className={styles.container}>
-     
-      <div className={styles.product}> 
-        {!products.length && <Loading />}
-        {products.map((product) => (
-          <ProductCards key={product.id} data={product} />
-        ))}
+    <>
+      <div>
+        <input
+          type="text"
+          placeholder="Search..."
+          value={search}
+          onChange={(e) => setSerach(e.target.value.toLocaleLowerCase().trim())}
+        />
+        <button onClick={ButtonHandler}>
+          <ImSearch />
+        </button>
       </div>
-    </div>
+      <div className={styles.container}>
+        <div className={styles.product}>
+          {!products.length && <Loading />}
+          {products.map((product) => (
+            <ProductCards key={product.id} data={product} />
+          ))}
+        </div>
+      </div>
+    </>
   );
 }
 
