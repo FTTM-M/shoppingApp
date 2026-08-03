309ccb6ea0e39179901e95dee8878a2c731a68d8
FTTM <fatemehalimirzaee14@gmail.com>
2026-08-03 12:48:01 +0330

    styled the sidebar

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -1,10 +1,9 @@
 import { FaListUl } from "react-icons/fa";
 import { createQuery } from "./helper/helper";
-import styles from "./SideBar.module.css"
+import styles from "./SideBar.module.css";
 
-function SideBar({setQuery}) {
-
-     const CategoriesHandler = (event) => {
+function SideBar({query, setQuery }) {
+  const CategoriesHandler = (event) => {
     const { tagName } = event.target;
 
     const category = event.target.innerText.toLowerCase();
@@ -13,6 +12,15 @@ function SideBar({setQuery}) {
 
     setQuery((query) => createQuery(query, { category }));
   };
+
+  const category = [
+    { id: 1, type: "All" },
+    { id: 2, type: "Electronics" },
+    { id: 3, type: "jewelery" },
+    { id: 4, type: "Men's Clothing" },
+    { id: 5, type: "Women's Clothing" },
+  ];
+
   return (
     <div className={styles.sidebar}>
       <div>
@@ -20,11 +28,11 @@ function SideBar({setQuery}) {
         <p>Categories</p>
       </div>
       <ul onClick={CategoriesHandler}>
-        <li>All</li>
-        <li>Electronics</li>
-        <li>jewelery</li>
-        <li>Men's Clothing</li>
-        <li>Women's Clothing</li>
+        {category.map((item) => (
+          <li key={category.id} className={item.type.toLowerCase() === query.category ? styles.selected : null}>
+            {item.type}
+          </li>
+        ))}
       </ul>
     </div>
   );
